8d16dc3d089ad593d6bae880a2f13f405a0a0673
dinosshan <dinosshan@birdsview.no>
2025-04-18 13:19:37 +0200

    Add minimal Neovim Lua config with lazy.nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,32 @@
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.termguicolors = true
+
+-- Keymap example
+vim.keymap.set('n', '<Space>', '<Nop>', { silent = true })
+
+-- Plugin manager (lazy.nvim)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins
+require("plugins")
+
+vim.cmd.colorscheme("tokyonight")
+
